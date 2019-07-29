64db1d2fa5a0b330eed025bf4039295b9e4abfcc
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-07-29 14:58:10 +0300

    Add extract_links.

---
+++ extract_links.py
@@ -0,0 +1,21 @@
+FILENAME_SOURCE = "linksintegrity.txt"
+EXCLUDED_KEYWORDS = ["threads active", "Check time", "Size"]
+FILENAME_OUTPUT = "links-output.txt"
+
+with open(FILENAME_SOURCE) as f:
+    content = f.readlines()
+
+# content = [x.strip() for x in content]
+
+
+def ok_line(line):
+    for keyword in EXCLUDED_KEYWORDS:
+        if keyword in line:
+            return False
+    return True
+
+
+with open(FILENAME_OUTPUT, 'a') as g:
+    for line in content:
+        if ok_line(line):
+            g.write(line)
